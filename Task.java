ec7a17c222a6a741f46cb01c2bf2a2f6eee1cf88
shunsuke5 <shunsuke1359@gmail.com>
2024-05-05 18:41:58 +0900

    20240505 18:41 例外処理未作成

--- Task.java
+++ Task.java
@@ -11,7 +11,7 @@ public class Task {
             while(!isEnd) {
                 displayList();
                 System.out.println("追加：1  完了:2  プログラムの終了：3\n");
-                System.out.print("番号を選択してください >> ");
+                System.out.print("行いたいアクションの番号を選択してください >> ");
                 Scanner scanner = new java.util.Scanner(System.in);
                 int choice = scanner.nextInt();
                 
@@ -45,12 +45,14 @@ public class Task {
 
     public void complete() {
         Scanner scanner = new java.util.Scanner(System.in);
-        System.out.print("\n完了したタスクを選択してください >> ");
+        System.out.print("\n完了したタスクの番号を選択してください >> ");
         int complete = scanner.nextInt();
-        for (int i = complete; i < this.taskCount; i++) {
+        // complete > this.taskCount、complete < 0のどちらかであれば、おかしい値なので例外処理へ
+        for (int i = complete; i <= this.taskCount; i++) {
             this.taskList[i] = this.taskList[i + 1];
         }
         System.out.println("タスクを完了しました");
+        // ここで実際にタスクがちゃんと消えているか確認を行わないと taskCount--; で結局タスクが消えている
         taskCount--;
     }
 
@@ -61,8 +63,10 @@ public class Task {
     }
 
     public void displayList() {
+        System.out.println("\n現在のタスク");
         for (int i = 0; i < this.taskCount; i++) {
-            System.out.println(this.taskList[i]);
+            System.out.println(this.taskList[i] + "：" + i);
         }
+        System.out.println("\n");
     }
 }
