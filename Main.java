7b86ab29efc681c0848d333ec1939efdbde3e22f
shunsuke5 <shunsuke1359@gmail.com>
2024-05-06 13:17:51 +0900

    20240506 13:17 完成

--- Main.java
+++ Main.java
@@ -1,22 +1,6 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         Task task = new Task();
         task.question();
-
-        /* Boolean isEnd = false;
-
-        while(!isEnd) {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            
-            System.out.println(a + b);
-            if (a + b == 10) {
-                isEnd = true;
-                scanner.close();
-            }
-        } */
     }
 }
